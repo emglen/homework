dc500b9d313d26d9a772915a1ff2e731afd809d0
homework1 <emglen@ukr.net>
2019-05-05 15:04:46 +0300

    Done: find sum of three natural numbers and write result in console.

---
+++ src/main/java/TreeNaturalNumbersSum.java
@@ -0,0 +1,16 @@
+public class TreeNaturalNumbersSum {
+
+    public static int numbersSum(int one, int two, int three){
+        return one+two+three;
+    }
+
+    public static void main(String[] args) {
+        int sum=numbersSum(10,3,7);
+        System.out.println("Sum of three natural numbers is: " + sum);
+
+        int value1=10;
+        int value2=3;
+        int value3=7;
+        System.out.println("Sum of " + value1 + " and " + value2 +  "and " + value3 + " is: " + numbersSum(value1,value2,value3));
+    }
+}
