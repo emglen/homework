ffa2e5f378cc7f5783f09c7f66be232202308ef2
homework1 <emglen@ukr.net>
2019-05-05 17:06:35 +0300

    Done: find factorial using loop

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+
+    public static int getFactorial(int value){
+        int factorial=1;
+        if(value==0){
+            return 1;
+        }
+        else {
+            for (int i=1;i<=value;i++) {
+              factorial=factorial*i;
+            }
+            return  factorial;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is: " + getFactorial(number));
+    }
+}
