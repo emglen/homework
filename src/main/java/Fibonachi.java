94fb788426be7361080d2c914a716023afcf90c4
homework1 <emglen@ukr.net>
2019-05-06 20:02:35 +0300

    Done: print in console numbers of fibonachi from first to n

--- src/main/java/Fibonachi.java
+++ src/main/java/Fibonachi.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Fibonachi {
 
-    public static void main(String[] args) {
+    public static void fibonachi(int fib){
+        int result=0;
+        int last=0;
+        int current=1;
 
+        System.out.print("Fibonachi numbers: "+ current + " ");
+        for(int i=1;i<fib;i++){
+            result=last+current;
+            System.out.print(result + " ");
+            last=current;
+            current=result;
+        }
+    }
+    public static int typeValue(){
+        Scanner inputValue=new Scanner(System.in);
+        int value=inputValue.nextInt();
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter n: ");
+        int n=typeValue();
+        fibonachi(n);
     }
 }
