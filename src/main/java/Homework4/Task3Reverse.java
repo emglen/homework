7c55c97dd00d0b706eec621c71f172b4c0946a1b
Виталий <z3p4dces46ky>
2019-06-02 18:41:31 +0300

    Done: Write a Java program to reverse a string

---
+++ src/main/java/Homework4/Task3Reverse.java
@@ -0,0 +1,9 @@
+package Homework4;
+
+public class Task3Reverse {
+    public static void main(String[] args) {
+    String str="quick brown fox jumps ";
+    String strReverse=new StringBuilder(str).reverse().toString();
+    System.out.println(strReverse);
+    }
+}
