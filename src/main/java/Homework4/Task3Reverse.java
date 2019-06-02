f7c0ba39eae209044ea088c275b978dbbade434f
Виталий <z3p4dces46ky>
2019-06-02 18:47:12 +0300

    Done: Write a Java program to reverse a string using loop

--- src/main/java/Homework4/Task3Reverse.java
+++ src/main/java/Homework4/Task3Reverse.java
@@ -1,9 +1,24 @@
 package Homework4;
 
 public class Task3Reverse {
+    public static String reverseMethod(String value) {
+        char[] chs = value.toCharArray();
+
+        int i=0, j=chs.length-1;
+        while (i < j) {
+            char t = chs[i];
+            chs[i] = chs[j];
+            chs[j] = t;
+            i++; j--;
+        }
+        return String.valueOf(chs);
+    }
+
     public static void main(String[] args) {
     String str="quick brown fox jumps ";
     String strReverse=new StringBuilder(str).reverse().toString();
     System.out.println(strReverse);
+
+        System.out.println(reverseMethod(str));
     }
 }
